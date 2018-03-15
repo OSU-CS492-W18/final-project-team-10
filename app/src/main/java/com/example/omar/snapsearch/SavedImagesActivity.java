7ebdc10954d9ac1f2ea903e2503a947cf3c5a619
Omar <omariltaf@hotmail.co.uk>
2018-03-15 15:35:41 -0700

    Created new activites again.

---
+++ app/src/main/java/com/example/omar/snapsearch/SavedImagesActivity.java
@@ -0,0 +1,13 @@
+package com.example.omar.snapsearch;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SavedImagesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_saved_images);
+    }
+}
