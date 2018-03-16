f83cdb9e61031217abd7e6c2512929f05465c456
Omar <omariltaf@hotmail.co.uk>
2018-03-15 17:31:37 -0700

    Completed Home screen functionality.

--- app/src/main/java/com/example/omar/snapsearch/MainActivity.java
+++ app/src/main/java/com/example/omar/snapsearch/MainActivity.java
@@ -1,13 +1,72 @@
 package com.example.omar.snapsearch;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.io.ByteArrayOutputStream;
 
 public class MainActivity extends AppCompatActivity {
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+
+    private Button mNewImageButton;
+    private Button mSavedImagesButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mNewImageButton = findViewById(R.id.btn_new_image);
+        mSavedImagesButton = findViewById(R.id.btn_saved_images);
+
+        mNewImageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dispatchTakePictureIntent();
+            }
+        });
+
+        mSavedImagesButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dispatchSavedImagesActivityIntent();
+            }
+        });
+    }
+
+    private void dispatchSavedImagesActivityIntent() {
+        Intent savedImagesActivityIntent = new Intent(this, SavedImagesActivity.class);
+        startActivity(savedImagesActivityIntent);
+    }
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+
+            ByteArrayOutputStream stream = new ByteArrayOutputStream();
+            imageBitmap.compress(Bitmap.CompressFormat.PNG, 100, stream);
+            byte[] byteArray = stream.toByteArray();
+
+            Intent singleImageActivityIntent = new Intent(this, SingleImageActivity.class);
+            singleImageActivityIntent.putExtra("image", byteArray);
+            startActivity(singleImageActivityIntent);
+        }
     }
 }
--- app/src/main/java/com/example/omar/snapsearch/SingleImageActivity.java
+++ app/src/main/java/com/example/omar/snapsearch/SingleImageActivity.java
@@ -1,13 +1,25 @@
 package com.example.omar.snapsearch;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class SingleImageActivity extends AppCompatActivity {
+
+    private ImageView mImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_single_image);
+
+        mImageView = findViewById(R.id.iv_image);
+
+        Bundle extras = getIntent().getExtras();
+        byte[] byteArray = extras.getByteArray("image");
+        Bitmap imageBitmap = BitmapFactory.decodeByteArray(byteArray, 0, byteArray.length);
+        mImageView.setImageBitmap(imageBitmap);
     }
 }
