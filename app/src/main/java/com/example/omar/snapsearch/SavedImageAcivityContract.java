40afb95b2fc959fb18b57b56d76e9cfab327fb32
Kenon <kahoanok@oregonstate.edu>
2018-03-22 19:41:13 -0700

    3/22 - Kenon - Added in SQL Database

---
+++ app/src/main/java/com/example/omar/snapsearch/SavedImageAcivityContract.java
@@ -0,0 +1,16 @@
+package com.example.omar.snapsearch;
+
+import android.provider.BaseColumns;
+
+/**
+ * Created by Kenon on 3/22/18.
+ */
+
+public class SavedImageAcivityContract {
+    private SavedImageAcivityContract() {}
+    public static class SavedImage implements BaseColumns{
+        final static String TABLE_NAME = "Saved Image";
+        final static String COLUMN_IMAGE_NAME = "name";
+        final static String COLUMN_IMAGE_BLOB = "image";
+    }
+}
---
+++ app/src/main/java/com/example/omar/snapsearch/SavedImageActivityDB.java
@@ -0,0 +1,37 @@
+package com.example.omar.snapsearch;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by Kenon on 3/22/18.
+ */
+
+public class SavedImageActivityDB extends SQLiteOpenHelper{
+    private static final String DATABASE_NAME = "SavedImage.db";
+    private static int DATABASE_VERSION = 1;
+
+    public SavedImageActivityDB(Context context){
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db){
+        final String SQL_CREATE_SAVED_IMAGE =
+                "CREATE TABLE " + SavedImageAcivityContract.SavedImage.TABLE_NAME + "(" +
+                SavedImageAcivityContract.SavedImage._ID + "INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                SavedImageAcivityContract.SavedImage.COLUMN_IMAGE_NAME + " TEXT NOT NULL, " +
+                SavedImageAcivityContract.SavedImage.COLUMN_IMAGE_BLOB + " BLOB" +
+                ");";
+        db.execSQL(SQL_CREATE_SAVED_IMAGE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion){
+        db.execSQL("DROP TABLE IF EXISTS " + SavedImageAcivityContract.SavedImage.TABLE_NAME);
+        onCreate(db);
+        onCreate(db);
+    }
+
+}
